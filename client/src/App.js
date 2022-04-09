6d567d3f4adea4642d4bb619db3fa64a98513e5e
Brian Mutinda <mutindabrian26@gmail.com>
2022-04-09 20:52:26 +0300

    get balance

--- client/src/App.js
+++ client/src/App.js
@@ -7,11 +7,13 @@ function App() {
   const [greet, setGreet] = useState("");
   const [greetingValue, setGreetingValue] = useState("");
   const [depositValue, setDepositValue] = useState("");
+  const [balance, setBalance] = useState();
 
   // A Web3Provider wraps a standard Web3 provider, which is
   // what MetaMask injects as window.ethereum into each page
   const provider = new ethers.providers.Web3Provider(window.ethereum);
   const signer = provider.getSigner();
+  const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
   useEffect(() => {
     const connectWallet = async () => {
@@ -19,7 +21,18 @@ function App() {
       await provider.send("eth_requestAccounts", []);
     };
 
+    // get wallet balance
+    const getBalance = async () => {
+      const balance = await provider.getBalance(
+        "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
+      );
+      const balanceFormatted = ethers.utils.formatEther(balance);
+      setBalance(balanceFormatted);
+    };
+
     connectWallet().catch(console.error);
+
+    getBalance().catch(console.error);
   });
 
   const handleDepositChange = (e) => {
@@ -44,7 +57,7 @@ function App() {
     <div className="w-full flex flex-col justify-center mt-4 p-4">
       <h1 className="text-3xl font-bold underline">Greeting</h1>
       <div className="flex flex-row justify-between w-3/4 space-x-2">
-        <div>Contract Balance: 0</div>
+        <div>Contract Balance: {balance}</div>
         <div>
           <div>
             <form onSubmit={handleDepositSubmit}>
