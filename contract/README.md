23d18ce0d07375b3310b0611434b4dd1736f04c7
Brian Mutinda <mutindabrian26@gmail.com>
2022-04-09 15:20:14 +0300

    setup and test

--- contract/README.md
+++ contract/README.md
@@ -1,4 +1,5 @@
 # Basic Sample Hardhat Project
+0x5FbDB2315678afecb367f032d93F642f64180aa3
 
 This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, a sample script that deploys that contract, and an example of a task implementation, which simply lists the available accounts.
 
--- contract/test/sample-test.js
+++ contract/test/sample-test.js
@@ -1,5 +1,6 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+const provider = waffle.provider;
 
 describe("Greeter", function () {
   it("Should return the new greeting once it's changed", async function () {
@@ -15,5 +16,14 @@ describe("Greeter", function () {
     await setGreetingTx.wait();
 
     expect(await greeter.greet()).to.equal("Hola, mundo!");
+  });
+
+  it("Should return the new balance after ether is deposited", async function () {
+    const Greeter = await ethers.getContractFactory("Greeter");
+    const greeter = await Greeter.deploy("Hello, world!");
+    await greeter.deployed();
+
+    await greeter.deposit({value: 10})
+    expect(await provider.getBalance(greeter.address)).to.equal(10);
   });
 });
