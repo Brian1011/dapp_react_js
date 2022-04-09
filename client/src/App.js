520e332244e801d55412ec42ad6a479c5feac599
Brian Mutinda <mutindabrian26@gmail.com>
2022-04-09 17:57:05 +0300

    add submit functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,31 @@
+import React, { useState, useEffect } from "react";
+import { ethers } from "ethers";
+
 function App() {
+  // declare a state with the variable greet
+  // use state -> current value and the function
+  const [greet, setGreet] = useState("");
+  const [greetingValue, setGreetingValue] = useState("");
+  const [depositValue, setDepositValue] = useState("");
+
+  const handleDepositChange = (e) => {
+    setDepositValue(e.target.value);
+  };
+
+  const handleGreetingChange = (e) => {
+    setGreetingValue(e.target.value);
+  };
+
+  const handleDepositSubmit = (e) => {
+    e.preventDefault();
+    console.log(depositValue);
+  };
+
+  const handleGreetingSubmit = (e) => {
+    e.preventDefault();
+    console.log(greetingValue);
+  };
+
   return (
     <div className="w-full flex flex-col justify-center mt-4 p-4">
       <h1 className="text-3xl font-bold underline">Greeting</h1>
@@ -6,25 +33,33 @@ function App() {
         <div>Contract Balance: 0</div>
         <div>
           <div>
-            <input
-              type="number"
-              className="py-2 px-2 border border-gray-500 rounded-md"
-            />
+            <form onSubmit={handleDepositSubmit}>
+              <input
+                type="number"
+                className="py-2 px-2 border border-gray-500 rounded-md"
+                onChange={handleDepositChange}
+                value={depositValue}
+              />
 
-            <div className="bg-blue-600 mt-2 text-white rounded-md flex justify-center">
-              <div className="py-2 px-2">Deposit</div>
-            </div>
+              <button className="bg-blue-600 mt-2 text-white rounded-md flex justify-center">
+                <div className="py-2 px-2">Deposit</div>
+              </button>
+            </form>
           </div>
 
           <div className="my-8">
-            <input
-              type="text"
-              className="py-2 px-2 border border-gray-500 rounded-md"
-            />
+            <form onSubmit={handleGreetingSubmit}>
+              <input
+                type="text"
+                className="py-2 px-2 border border-gray-500 rounded-md"
+                onChange={handleGreetingChange}
+                value={greetingValue}
+              />
 
-            <div className="bg-black mt-2 text-white rounded-md flex justify-center">
-              <div className="py-2 px-2">Change</div>
-            </div>
+              <button className="bg-black mt-2 text-white rounded-md flex justify-center">
+                <div className="py-2 px-2">Change</div>
+              </button>
+            </form>
           </div>
         </div>
       </div>
